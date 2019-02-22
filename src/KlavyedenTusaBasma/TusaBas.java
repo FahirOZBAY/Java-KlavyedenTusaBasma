19f872637d249d54e3e98e2bce06e84bc15c9308
Fahir ÖZBAY <ratapan@gmail.com>
2019-02-22 23:59:11 +0300

    JavaIleKlavyedenTusaBasma

---
+++ src/KlavyedenTusaBasma/TusaBas.java
@@ -0,0 +1,26 @@
+package KlavyedenTusaBasma;
+
+import java.awt.AWTException;
+import java.awt.HeadlessException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+public class TusaBas {
+
+	public static void main(String[] args) {
+
+		try {
+			boolean isPressed = false;
+			Robot robot = new Robot();
+			if (!isPressed)
+				robot.keyPress(KeyEvent.VK_PRINTSCREEN);
+			robot.keyRelease(KeyEvent.VK_PRINTSCREEN);
+			isPressed = true;
+		} catch (HeadlessException | AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
